e536afadd4183ed71d91fd150ca5effe57c63e82
Marina <marishka-2008-91@mail.ru>
2022-12-14 21:22:39 +0300

    7

--- controller.py
+++ controller.py
@@ -4,4 +4,4 @@ import routiner as rt
 def button_click(flag):
     first_numb, second_numb, numb_oper = vr.menu_collection(flag)
     res = rt.calc(first_numb, second_numb, numb_oper)
-    vr.view_res(flag, res)
+    vr.view_res(flag, res) 
